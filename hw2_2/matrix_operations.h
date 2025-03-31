6dfbe3681e1732d645e672a2b5354786f2d95622
ValeryiaZ-dev <ValeryZvereva@yandex.ru>
2025-03-31 13:09:29 +0300

    Update matrix_operations.h

--- hw2_2/matrix_operations.h
+++ hw2_2/matrix_operations.h
@@ -3,7 +3,7 @@
 
 #include <iostream>
 
-void multiplyMatrices(int firstMatrix[][10], int secondMatrix[][10], int result[][10], int rowFirst, int columnFirst, int rowSecond, int columnSecond);
-void multiplyMatrixByVector(int matrix[][10], int vector[], int result[], int row, int column);
+void multiplyMatrices(int firstMatrix[][512], int secondMatrix[][512], int result[][512], int rowFirst, int columnFirst, int rowSecond, int columnSecond);
+void multiplyMatrixByVector(int matrix[][512], int vector[], int result[], int row, int column);
 
 #endif // MATRIX_OPERATIONS_H
