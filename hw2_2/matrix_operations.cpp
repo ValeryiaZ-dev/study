66f032021e049fbc4e2f13dd2609a0db242ffd48
ValeryiaZ-dev <ValeryZvereva@yandex.ru>
2025-03-31 13:09:17 +0300

    Update matrix_operations.cpp

--- hw2_2/matrix_operations.cpp
+++ hw2_2/matrix_operations.cpp
@@ -1,6 +1,6 @@
 #include "matrix_operation.h"
 
-void multiplyMatrices(int firstMatrix[][10], int secondMatrix[][10], int result[][10], int rowFirst, int columnFirst, int rowSecond, int columnSecond) {
+void multiplyMatrices(int firstMatrix[][512], int secondMatrix[][512], int result[][512], int rowFirst, int columnFirst, int rowSecond, int columnSecond) {
     // Инициализация результата нулями
     for (int i = 0; i < rowFirst; ++i)
         for (int j = 0; j < columnSecond; ++j)
@@ -14,7 +14,7 @@ void multiplyMatrices(int firstMatrix[][10], int secondMatrix[][10], int result[
             }
 }
 
-void multiplyMatrixByVector(int matrix[][10], int vector[], int result[], int row, int column) {
+void multiplyMatrixByVector(int matrix[][512], int vector[], int result[], int row, int column) {
     // Инициализация результата нулями
     for (int i = 0; i < row; ++i)
         result[i] = 0;
